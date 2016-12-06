4a8957faeb23e1ba26c7750efb64eee627866b98
Olivia Boertje <malevolentninja@gmail.com>
2016-12-06 15:54:39 +0000

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,45 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+
+
+var firstname =  "Olivia";
+
+var age = "22";
+
+console.log(firstname);
+
+
+
+var email = "olivia@gmail.com";
+
+var newEmail =
+    email.replace("olivia", "Oliviaboertje");
+
+console.log(email);
+conosle.log(newEmail);
+
+
+var awesomeThoughts = "My name is Olivia and I am AWESOME!";
+
+console.log(awesomeThoughts);
+
+
+var funThoughts =
+
+    awesomeThoughts.replace("AWESOME", "FUN");
+
+
+
+ $("#main").append(funThoughts);
+
+
+
+
+var name = "Olivia";
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+
+var role = "Web Developer";
+var formattedRole = HTMLheaderRole.replace("%data%", role );
+
+
+$("#header").prepend(formattedName);
+$("#header").prepend(formattedRole);
